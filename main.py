f249fc7afd53b0e093b60aaac856f6abc83585a9
BDM <95568319+BDEMAY0@users.noreply.github.com>
2023-09-11 23:11:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     # Initialisation du parser d'arguments
     parser = argparse.ArgumentParser(description="FFF - Find Forensic Faster.")
     parser.add_argument("-f", "--file", required=True, help="Chemin vers le fichier dump à analyser.")
-    parser.add_argument("-p", "--path", default="volatility3-develop",
+    parser.add_argument("-p", "--path", default="volatility3",
                         help="Chemin vers le dossier qui contient vol.py de Volatility. Par défaut: 'volatility3-develop'")
     parser.add_argument("-os", "--ostype", default="windows",
                         help="Le type du système d'exploitation: 'windows' ou 'linux'.'")
