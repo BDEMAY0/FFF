1b25d9e6216384618623988ffc664e3c53c315a2
BDM <95568319+BDEMAY0@users.noreply.github.com>
2023-09-11 22:57:49 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import argparse
+from FFF.Package_Windows.ManagePlugin import ManagePlugin
+from FFF.Package_Windows.ManageRegister import ManageRegister
+
+def main():
+    # Initialisation du parser d'arguments
+    parser = argparse.ArgumentParser(description="FFF - Find Forensic Faster.")
+    parser.add_argument("-f", "--file", required=True, help="Chemin vers le fichier dump à analyser.")
+    parser.add_argument("-p", "--path", default="volatility3-develop",
+                        help="Chemin vers le dossier qui contient vol.py de Volatility. Par défaut: 'volatility3-develop'")
+    parser.add_argument("-os", "--ostype", default="windows",
+                        help="Le type du système d'exploitation: 'windows' ou 'linux'.'")
+
+    args = parser.parse_args()
+
+    if args.ostype == 'windows':
+        register = ManageRegister(args)
+        # register.execute()
+        plugins = ManagePlugin(args)
+        plugins.execute()
+
+    elif args.ostype == 'linux':
+        print("en cours de travaux")
+        #putsomecode
+
+    else:
+        print("Wrong ostype")
+
+
+if __name__ == "__main__":
+    main()
